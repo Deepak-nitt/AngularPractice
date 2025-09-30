a19c92e9606057153fecf52308209cd5268f9568
Deepak-nitt <deepak.kumar@cleaff.com>
2025-10-01 00:30:42 +0530

    ended all

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,10 +6,14 @@ import { Calculator } from "./components/calculator/calculator";
 import { Showpassword } from "./components/showpassword/showpassword";
 import { RandomQuotes } from "./components/random-quotes/random-quotes";
 import { Modal } from './components/modal/modal';
+import { Accordion } from "./components/accordion/accordion";
+
+import { Darkmode } from "./components/darkmode/darkmode";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Livetext, Todos, Calculator, Showpassword, RandomQuotes,Modal],
+  imports: [RouterOutlet, Livetext, Todos, Calculator, Showpassword, RandomQuotes, Modal, Accordion, Darkmode],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/darkmode/darkmode.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-darkmode',
+  imports: [],
+  templateUrl: './darkmode.html',
+  styleUrl: './darkmode.css'
+})
+export class Darkmode {
+
+}
